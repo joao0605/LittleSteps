6a0dcb541224003fa8a86008b74c2cd0e47a1ae7
unknown <joaofernandes0605@gmail.com>
2023-06-05 18:04:47 +0100

    endpoint

--- src/pages/api/manager/forms/index.js
+++ src/pages/api/manager/forms/index.js
@@ -1,6 +1,12 @@
+import { newForm } from "@/controllers/FormController";
 
 
 
 export default async function (req, res) {
-    
+
+    if( req.method ==="POST"){
+        newForm(req, res)
+    }
+
+
 }
