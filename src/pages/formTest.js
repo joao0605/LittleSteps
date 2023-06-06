d513fec3cc92b117f8aae7f6b3ee297f8de79adb
joao0605 <joaofernandes0605@gmail.com>
2023-06-06 11:50:26 +0100

    authentication with no bugs

--- src/pages/formTest.js
+++ src/pages/formTest.js
@@ -2,10 +2,19 @@ import BigMenuButton from "@/components/buttons/bigMenuButton";
 import Form from "@/components/form/form";
 import NavButton from "@/components/navButton/navButtonStudent";
 import TopBar from "@/components/navButton/topBar";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 
 export default function formTest() {
+    const router = useRouter()
 
+    useEffect(() => {
+        const logged = Boolean(localStorage.getItem('token'))
+        if (!logged) {
+            router.push('/login')
+        }
+    }, [])
     
     //aqui vamos verificar se o perfil é do tipo pai ou responsável e enviar a resposta como props, para modificar o formulario
     return (
@@ -19,24 +28,24 @@ export default function formTest() {
     )
 }
 
-export const getServerSideProps = async ({ req, res }) => {
-    try {
-      const token = getCookie('authorization', { req, res }); 
-      if (!token) {
-        throw new Error('Token Inválido');
-      }
-      authorize(token);
+// export const getServerSideProps = async ({ req, res }) => {
+//     try {
+//       const token = getCookie('authorization', { req, res }); 
+//       if (!token) {
+//         throw new Error('Token Inválido');
+//       }
+//       authorize(token);
   
-      return {
-        props: {}
-      };
-    } catch (err) {
-      return {
-        redirect: {
-          permanent: false,
-          destination: '/login'
-        },
-        props: {}
-      };
-    }
-  };
+//       return {
+//         props: {}
+//       };
+//     } catch (err) {
+//       return {
+//         redirect: {
+//           permanent: false,
+//           destination: '/login'
+//         },
+//         props: {}
+//       };
+//     }
+//   };
--- src/pages/personalDataStudentTest.js
+++ src/pages/personalDataStudentTest.js
@@ -1,8 +1,12 @@
 import NavButton from "@/components/navButton/navButtonStudent";
 import TopBar from "@/components/navButton/topBar";
 import PersonalDataStudent from "@/components/personalDataStudent/personalDataStudent";
+import { useEffect } from "react";
 
 export default function personalDataStudebtTest() {
+
+    useEffect()
+    
 
     //aqui vamos verificar se o perfil é do tipo pai ou responsável e enviar a resposta como props, para modificar o formulario
     return (
