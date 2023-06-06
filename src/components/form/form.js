b2af8ca35b64d0ba57f0bf2d2d1622834eb2f4f2
Daniel Barreiras <carlassmedina@hotmail.com>
2023-06-06 11:00:18 +0100

    testeCarla

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,64 +1,14 @@
-import BathroomBox from "./bathroomBox";
-import ReportBox from "./reportBox";
-import BreakfastBox from "./breakfastBox";
-import LunchBox from "./lunchBox";
-import SleepBox from "./sleepBox";
-import CommentBox from "./commentBox";
-import { useState } from "react";
+import RangeSlider from "./rangeSlider";
 
+export default function Form(){
 
-
-
-export default function Form() {
-
-    // const [formData, setFormData] = useState({
-    //     date: new Date(),
-    //     breakfast: 2,
-    //     nap: 3 
-    // })
-
-    // function handleClick() {
-    //     fetch ("/api/maganer/form", {
-    //         method: "POST",
-    //         headers: {
-    //             "Content-Type": "application/json",
-    //         },
-    //         body: JSON.stringify(formData)
-    //     })
-    //     if (resposnse.status == 200){
-    //         setFormData(
-    //             {
-    //                 date: new Date(),
-    //                 breakfast: 2,
-    //                 nap: 3 
-    //             }
-    //         )
-    //     }
-    //     return response.json()
-    // }
-    const relatorio = {
-        name: "Joao",
-        date: "05/06/23"
-    }
-   
-    return (
-        <div>          
-            <ReportBox date={relatorio.date} name={relatorio.name} />
-            <div>
-                <BreakfastBox />
-            </div>
-            <div>
-                <LunchBox />
-            </div>
-            <div>
-                <BathroomBox />
-            </div>
-            <div>
-                <SleepBox />
-            </div>
-            <CommentBox />
-
-
+    return(
+        <div>
+            <RangeSlider/>
+            <RangeSlider/>
+            <RangeSlider/>
+            <RangeSlider/>
+            <RangeSlider/>
         </div>
     )
-}//  <button onClick={handleClick() }></button>
+}
