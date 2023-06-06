40542b0ce4cc324f190af3fb92e54a373f067c87
unknown <quaresmaamanda4@gmail.com>
2023-06-06 17:38:59 +0100

    mongoose solved

---
+++ src/database/conexãoMongo.js
@@ -0,0 +1,15 @@
+                import mongoose from "mongoose";
+
+                const connectDB = async () => {
+                try {
+                    await mongoose.connect("mongodb+srv://quaresmaamanda4:3EYWFQMF2cnBKSmC@mandy.fhynimk.mongodb.net/", {
+                    useNewUrlParser: true,
+                    useUnifiedTopology: true,
+                    });
+                    console.log("Conectado ao MongoDB");
+                } catch (error) {
+                    console.error("Erro ao conectar ao MongoDB", error);
+                }
+                };
+
+                export default connectDB;
