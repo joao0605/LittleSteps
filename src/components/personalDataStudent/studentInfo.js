895e676ae785dcacb61f633c2d5aa72e3f8698ab
Daniel Barreiras <carlassmedina@hotmail.com>
2023-06-07 16:31:38 +0100

    new routes and pages

--- src/components/personalDataStudent/studentInfo.js
+++ src/components/personalDataStudent/studentInfo.js
@@ -1,12 +1,12 @@
 import styles from './StudentInfo.module.css'
 
-export default function StudentInfo({name, birthday}) {
+export default function StudentInfo({name, birthdate}) {
 
     return (
         <div>
             <h2 className={styles.title}><strong>Informações Aluno</strong></h2>
             <p className={styles.stText}>Nome: {name}</p>
-            <p className={styles.text}>Data de nascimento: {birthday}</p>
+            <p className={styles.text}>Data de nascimento: {birthdate}</p>
         </div>
     )
 }
--- src/pages/listStudents.js
+++ src/pages/listStudents.js
@@ -3,7 +3,8 @@ import NavButtonTeacher from "@/components/navButton/navButtonTeacher"
 import List from "@/components/list/list"
 
 import { useRouter } from "next/router"
-import Item from "@/components/list/itens"
+import { ItemStudents } from "@/components/list/itens"
+
 
 export default function listStudents() {
 
@@ -37,14 +38,19 @@ export default function listStudents() {
     const router = useRouter()
 //onClick tem que chamar uma function
 
+// dentro da Div junto com LIST colocar a imagem do calendario com onClick que direciona para o historico de relatorios
+// colocar a rota para ao clicar no nome/id ir para a pagina com as informações do aluno
+
+
     return (
         <div>
             <TopBar />
             <NavButtonTeacher />
+            <div>
             <List>
-                {itens.map(i => <Item onClick={() => router.push(i.registration)} name={i.name} registration={i.registration} />)}
+                {itens.map(i => <ItemStudents onClick={() => router.push('/personalDataStudentTest')} name={i.name} registration={i.registration} />)}
             </List>
-            
+            </div>
 
         </div>
     )
